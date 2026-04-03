e46c2fa0182c6c72dec266a2525df8847f23745f
monika khade <monikakhade2013@gmail.com>
2026-04-03 15:15:47 +0530

    add token generation to signup and login controllers; create generateToken utility

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,9 +1,10 @@
 import { User } from "../model/user.model.js"
 import bcrypt from "bcrypt"
+import { generateToken } from "../utiles/generateToken.js"
 export const signupController = async (req,res)=>{
     try {
         const {username, email, password} = req.body
-        if(!username || !password ||! email){
+        if(!username || !password ||!email){
             return res.json({message:"All fields are required"})
         }
         const user = await User.findOne({email})
@@ -16,16 +17,28 @@ export const signupController = async (req,res)=>{
             password:hashedPassword,
             email
         })
-        return res.json({message:"user created successfully", data:newUser})
+        const token = await generateToken(user)
+        return res.json({message:"user created successfully", data:newUser, token:token})
     } catch (error) {
         res.status(500).json({message:"Error in signupController",error: error.message})
         console.log("Error in signupController", error.message);
-        
     }
+
 }
 export const loginController = async (req,res)=>{
     try {
-        
+        const {email, password} = req.body
+        const user = await User.findOne({email})
+        if(!user){
+             return res.json({message:"User doesn't exist"})
+        }
+        const matchPassword = await bcrypt.compare(password,user.password)
+        const token = await generateToken(user)
+
+        if(matchPassword && user){
+            return res.json({message:"Login succesfully", data:user, token:token})
+        }
+
     } catch (error) {
         res.status(500).json({message:"Error in loginController",error: error.message})
         console.log("Error in loginController", error.message);
