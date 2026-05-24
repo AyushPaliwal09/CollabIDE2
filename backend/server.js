9e8ede2791cdc5b27215b8d52c3115807b2842d0
AyushPaliwal09 <ayushpailwal595@gmail.com>
2026-05-24 21:13:17 +0530

    fix: update CORS configuration to include methods and allowed headers

--- backend/server.js
+++ backend/server.js
@@ -118,13 +118,15 @@ socket.on("chat-message", ({ roomId, message, user }) => {
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors());
 app.use(cors({
   origin: [
   "http://localhost:5173",
   "https://collabide-sjuu.onrender.com/"],
-  credentials: true
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
+app.use(cors());
 app.use("/api/auth", userRouter)
 app.use("/room", roomRouter)
 app.get("/",(req,res)=> res.send("Hello there"))
