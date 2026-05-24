5ef02a2787d8f80a732e5e86600fb7b4a18957f7
AyushPaliwal09 <ayushpailwal595@gmail.com>
2026-05-24 22:13:41 +0530

    fix: comment out allowedHeaders in CORS configuration

--- backend/server.js
+++ backend/server.js
@@ -124,7 +124,7 @@ app.use(cors({
   "https://collabide-sjuu.onrender.com/"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  // allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.set("trust proxy", 1);
 app.use(cors());
